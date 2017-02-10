2a601f860c17b65c82d3e7333d1dbb10908e7e88
DavidBetteridge <davidabetteridge@gmail.com>
2017-02-10 23:37:31 +0000

    Designer fixes

--- PuzzleLevelDesigner/MainWindow.xaml.cs
+++ PuzzleLevelDesigner/MainWindow.xaml.cs
@@ -35,7 +35,9 @@ namespace PuzzleLevelDesigner
         private Cell teleport1;
 
         //Keep track of the player
-        private MapCell player;  
+        private MapCell player;
+
+        public MapCell[,] MapCells { get; set; }  //x,y
 
         public MainWindow()
         {
@@ -59,6 +61,8 @@ namespace PuzzleLevelDesigner
             map.Rows = board.Width;
             map.Columns = board.Height;
 
+            this.MapCells = new MapCell[board.Width, board.Height];
+
             for (int y = 0; y < map.Rows; y++)
             {
                 for (int x = 0; x < map.Columns; x++)
@@ -71,6 +75,8 @@ namespace PuzzleLevelDesigner
                         X = x,
                         Y = y
                     };
+                    this.MapCells[x, y] = mapCell;
+
 
                     if (cell.Contents as Teleport != null) mapCell.ImageSource = this.teleportImg;
                     if (cell.Contents as Player != null)
@@ -225,55 +231,84 @@ namespace PuzzleLevelDesigner
 
                 // Top Walls
                 if (this.addTopWall.IsChecked == true)
-                {
-                    cell.HasTopWall = true;
-                    mc.TopWall = true;
-                }
+                    AddTopWall(mc, cell, true);
 
                 if (this.removeTopWall.IsChecked == true)
-                {
-                    cell.HasTopWall = false;
-                    mc.TopWall = false;
-                }
+                    AddTopWall(mc, cell, false);
 
                 // Left Walls
                 if (this.addLeftWall.IsChecked == true)
-                {
-                    cell.HasLeftWall = true;
-                    mc.LeftWall = true;
-                }
+                    AddLeftWall(mc, cell, true);
 
                 if (this.removeLeftWall.IsChecked == true)
-                {
-                    cell.HasLeftWall = false;
-                    mc.LeftWall = false;
-                }
+                    AddLeftWall(mc, cell, false);
 
                 // Right Walls
                 if (this.addRightWall.IsChecked == true)
-                {
-                    cell.HasRightWall = true;
-                    mc.RightWall = true;
-                }
+                    AddRightWall(mc, cell, true);
 
                 if (this.removeRightWall.IsChecked == true)
-                {
-                    cell.HasRightWall = false;
-                    mc.RightWall = false;
-                }
+                    AddRightWall(mc, cell, false);
 
                 // Bottom Walls
                 if (this.addBottomWall.IsChecked == true)
-                {
-                    cell.HasBottomWall = true;
-                    mc.BottomWall = true;
-                }
+                    AddBottomWall(mc, cell, true);
 
                 if (this.removeBottomWall.IsChecked == true)
-                {
-                    cell.HasBottomWall = false;
-                    mc.BottomWall = false;
-                }
+                    AddBottomWall(mc, cell, false);
+            }
+        }
+
+        private void AddTopWall(MapCell mc, Cell cell, bool add)
+        {
+            if (cell.Y > 0)
+            {
+                cell.HasTopWall = add;
+                mc.TopWall = add;
+
+                // Add a bottom wall to the cell above
+                var cell2 = this.MapCells[cell.X, cell.Y - 1];
+                cell2.BottomWall = add;
+                this.board.Cells[cell.X, cell.Y - 1].HasBottomWall = add;
+            }
+        }
+
+        private void AddLeftWall(MapCell mc, Cell cell, bool add)
+        {
+            if (cell.X > 0)
+            {
+                cell.HasLeftWall = add;
+                mc.LeftWall = add;
+
+                var cell2 = this.MapCells[cell.X - 1, cell.Y];
+                cell2.RightWall = add;
+                this.board.Cells[cell.X - 1, cell.Y].HasRightWall = add;
+            }
+        }
+
+        private void AddRightWall(MapCell mc, Cell cell, bool add)
+        {
+            if (cell.X + 1 < this.board.Width)
+            {
+                cell.HasRightWall = add;
+                mc.RightWall = add;
+
+                var cell2 = this.MapCells[cell.X + 1, cell.Y];
+                cell2.LeftWall = add;
+                this.board.Cells[cell.X + 1, cell.Y].HasLeftWall = add;
+            }
+        }
+
+        private void AddBottomWall(MapCell mc, Cell cell, bool add)
+        {
+            if (cell.Y + 1 < this.board.Height)
+            {
+                cell.HasBottomWall = add;
+                mc.BottomWall = add;
+
+                var cell2 = this.MapCells[cell.X, cell.Y + 1];
+                cell2.TopWall = add;
+                this.board.Cells[cell.X, cell.Y + 1].HasTopWall = add;
             }
         }
 
